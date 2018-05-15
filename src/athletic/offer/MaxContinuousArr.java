8bba09530edd2eece6bfdfffba5cffd9882b6ea2
huixing <121674028@qq.com>
2018-05-15 15:59:17 +0800

    修改错误，需要用两个数字来标记长度

--- src/athletic/offer/MaxContinuousArr.java
+++ src/athletic/offer/MaxContinuousArr.java
@@ -4,7 +4,7 @@ public class MaxContinuousArr {
 
     public static void main(String[] args) {
 
-        int [] arr = new int[]{-2,-3,-5,1,3,-1,2,9,-10,8,3,-4,-5,20};
+        int [] arr = new int[]{-2,-3,5,-1,3,-1,-2,9,-10,8,3,4,5,-20};
         int i = maxContinus(arr);
 
         System.out.println("求的最大值为："+i);
@@ -19,23 +19,26 @@ public class MaxContinuousArr {
         //开始位置，如果需要的话
         int endposition = 0;
         //结束位置，如果需要的话
-        int startposition = 0;
+        int maxlength = 0;
+        int nowLength = 0;
 
         for (int i=0;i<arr.length;i++){
 
             localMax += arr[i];
+            nowLength ++;
 
             if (localMax > maxcount){
                 maxcount = localMax;
+                maxlength = nowLength;
                 endposition = i;
             }else if (localMax < 0){
                 localMax = 0;
-                startposition = i+1;
+                nowLength = 0;
             }
 
         }
 
-        System.out.println("从这个位置开始："+startposition);
+        System.out.println("数据长度为："+maxlength);
         System.out.println("到这个位置："+endposition);
 
 
